dcb3a1ca366d6aa3097713df3b9c28070889e51d
kristynerhaugen <kristynerhaugen@gmail.com>
2022-12-13 16:37:41 -0600

    basics of header set up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
+import Header from "./components/Header";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Header will go here.</p>
-      </header>
-      <div>Knife River Sawmill</div>
+    <div>
+      <Header />
+      <div>
+        <h3>Contact us for your sawmill needs!</h3>
+        <h3>(123)-456-7890</h3>
+        <h3>
+          <a href="mailto:kristynerhaugen@gmail.com">
+            EmailExample@example.com
+          </a>
+        </h3>
+      </div>
     </div>
   );
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Knife River Sawmill</h1>
+      <h2>About Us</h2>
+      <h2>Our Work</h2>
+    </header>
+  );
+}
+
+export default Header;
