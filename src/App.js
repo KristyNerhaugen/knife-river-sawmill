8a2f4a15bd6e82f142c7b2da8941cdde945a6d16
kristynerhaugen <kristynerhaugen@gmail.com>
2022-12-13 16:52:26 -0600

    footer set up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 import "./App.css";
 
@@ -16,6 +17,7 @@ function App() {
           </a>
         </h3>
       </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>© Kristy Nerhaugen 2022</p>
+    </footer>
+  );
+}
+
+export default Footer;
